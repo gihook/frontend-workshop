b89a5e427b5c78f74c3b4bd9c21367b74931e857
Nikola Djukanovic <nikoladjukanovic1989@gmail.com>
2022-02-22 12:29:53 +0100

    wip: basic observable shell

--- index.ts
+++ index.ts
@@ -1,1 +1,85 @@
-console.log("Hello world");
+declare type Callback = (x: any) => void;
+
+interface Observer {
+  next: (x: any) => void;
+  complete: () => void;
+  error: (error: any) => void;
+}
+
+interface Subscription {
+  unsubscribe: () => void;
+}
+
+const test$ = {
+  subscribe: function (observer: Partial<Observer>): Subscription {
+    const next = observer.next || function () {};
+    const complete = observer.complete || function () {};
+
+    next(1);
+    next(2);
+    next(3);
+    next(4);
+    next(5);
+
+    setTimeout(() => {
+      next(6);
+      complete();
+    }, 0);
+
+    const timeoutReference1 = setTimeout(() => {
+      next(7);
+      complete();
+    }, 0);
+
+    return {
+      unsubscribe: function () {
+        clearTimeout(timeoutReference1);
+        console.log("unsubscribed");
+      },
+    };
+  },
+};
+
+const interval$ = {
+  subscribe: function (observer: Partial<Observer>): Subscription {
+    const next = observer.next || function () {};
+
+    let i = 0;
+    const r = setInterval(() => next(i++), 100);
+
+    return {
+      unsubscribe: function () {
+        clearInterval(r);
+      },
+    };
+  },
+};
+
+const testPromise = new Promise((resolve, _reject) => {
+  setTimeout(() => resolve(1), 1000);
+});
+
+const fromPromise$ = {
+  subscribe: function (observer: Partial<Observer>): Subscription {
+    const next = observer.next || function () {};
+    const complete = observer.complete || function () {};
+
+    testPromise.then((b) => {
+      next(b);
+      complete();
+    });
+
+    return {
+      unsubscribe: function () {},
+    };
+  },
+};
+
+const subscription = fromPromise$.subscribe({
+  next: function (x) {
+    console.log({ x });
+  },
+  complete: function () {
+    console.log("complete");
+  },
+});
